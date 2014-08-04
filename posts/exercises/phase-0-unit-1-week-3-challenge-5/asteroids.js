0ef7d453b7a286933840b7337a695946fa71ec1f
Dustin Snyder <roboprog010@gmail.com>
2014-08-03 20:11:12 -0500

    Add recent changes to asteroids game.

--- posts/exercises/phase-0-unit-1-week-3-challenge-5/asteroids.js
+++ posts/exercises/phase-0-unit-1-week-3-challenge-5/asteroids.js
@@ -1,74 +1,79 @@
+//I am going to try and pseudocode from scratch and compare to my code here next week.  Sorry I didn't do this first I got a little carried away.  I was just excited to make my first OOP!
+
 $(document).ready(function(){
 	var map = new Map();
-	var ship = new SpaceShip();
-	var weaponBlasts = {
-		counter: 0,
-		list: {}
-	};
-	var asteroids = {
-		counter: 0,
-		list: {}
-	};
-
-	for(var i = 0; i < 8; i++) {
-		asteroids.counter++;
-		asteroids.list['a' + asteroids.counter] = new Asteroid('a' + asteroids.counter, map.level);
-	}
-
-	window.setInterval(function(){
-		ship.move();
-		for (id in weaponBlasts.list) {
-			weaponBlasts.list[id].move(weaponBlasts);
-		}
-		for (id in asteroids.list) {
-			asteroids.list[id].move();
-		}
-		collision_test(ship, weaponBlasts, asteroids, WEAPON_BLAST_SIZE, ASTEROID_SIZE, map);
-	}, 33);
-
-	$(document).keydown(function(e){
-		switch (e.which) {
-			case 37: //left arrow
-				ship.accelerate('l');
-				e.preventDefault();
-				break;
-			case 38: //up arrow
-				ship.accelerate('u');
-				e.preventDefault();
-				break;
-			case 39: //right arrow
-				ship.accelerate('r');
-				e.preventDefault();
-				break;
-			case 40: //down arrow
-				ship.accelerate('d');
-				e.preventDefault();
-				break;
-			case 32: //space bar
-				ship.fire(weaponBlasts);
-				e.preventDefault();
-				break;
-		}
-	});
-
-	function collision_test(ship, weaponBlasts, asteroids, WEAPON_BLAST_SIZE, ASTEROID_SIZE, map) {
-		collisionLoop: for (wbId in weaponBlasts.list) {
-			for (aId in asteroids.list) {
-				wbXMin = weaponBlasts.list[wbId].position.x;
-				wbXMax = weaponBlasts.list[wbId].position.x + WEAPON_BLAST_SIZE;
-				wbYMin = weaponBlasts.list[wbId].position.y;
-				wbYMax = weaponBlasts.list[wbId].position.y + WEAPON_BLAST_SIZE;
-				aXMin = asteroids.list[aId].position.x;
-				aXMax = asteroids.list[aId].position.x + ASTEROID_SIZE;
-				aYMin = asteroids.list[aId].position.y;
-				aYMax = asteroids.list[aId].position.y + ASTEROID_SIZE;
-				if (!(wbXMax < aXMin || aXMax < wbXMin || wbYMax < aYMin || aYMax < wbYMin)) {
-					weaponBlasts.list[wbId].evaporate(weaponBlasts);
-					asteroids.list[aId].evaporate(asteroids);
-					map.increaseScore();
-					break collisionLoop;
-				}
+	$('#MessageBoard').html(map.messageBoard.start);
+	$('#start').click(function(){
+		$('#MessageBoard').css('left','-9999em');
+		map.initializeLevel();
+		timingFunction = window.setInterval(function(){
+			map.ship.move();
+			for (id in map.weaponBlasts.list) {
+				map.weaponBlasts.list[id].move(map);
 			}
-		}
-	}
+			for (id in map.asteroids.list) {
+				map.asteroids.list[id].move();
+			}
+			map.collisionTest();
+		}, 33);
+
+		$(document).keydown(function(e){
+			switch (e.which) {
+				case 37: //left arrow
+					map.ship.accelerate('l');
+					e.preventDefault();
+					break;
+				case 38: //up arrow
+					map.ship.accelerate('u');
+					e.preventDefault();
+					break;
+				case 39: //right arrow
+					map.ship.accelerate('r');
+					e.preventDefault();
+					break;
+				case 40: //down arrow
+					map.ship.accelerate('d');
+					e.preventDefault();
+					break;
+				case 32: //space bar
+					map.ship.fire(map);
+					e.preventDefault();
+					break;
+			}
+		});
+		$('#Pause').click(function(){ 
+			$('#Play').removeClass('active');
+			window.clearInterval(timingFunction); 
+		});
+		$('#Play').click(function(){ 
+			if(!$(this).hasClass('active')) {
+				$(this).addClass('active');
+				timingFunction = window.setInterval(function(){
+					map.ship.move();
+					for (id in map.weaponBlasts.list) {
+						map.weaponBlasts.list[id].move(map);
+					}
+					for (id in map.asteroids.list) {
+						map.asteroids.list[id].move();
+					}
+					map.collisionTest();
+				}, 33);
+			}
+		 });
+		$('#Restart').click(function(){ 
+			$('#Play').removeClass('active');
+			map.restart(); 
+			window.clearInterval(timingFunction); 
+			timingFunction = window.setInterval(function(){
+				map.ship.move();
+				for (id in map.weaponBlasts.list) {
+					map.weaponBlasts.list[id].move(map);
+				}
+				for (id in map.asteroids.list) {
+					map.asteroids.list[id].move();
+				}
+				map.collisionTest();
+			}, 33);
+		});
+	});
 });
--- posts/exercises/phase-0-unit-1-week-3-challenge-5/asteroids_classes.js
+++ posts/exercises/phase-0-unit-1-week-3-challenge-5/asteroids_classes.js
@@ -8,39 +8,130 @@ var ASTEROID_WAVES = 4;
 function Map() {
 	this.width = MAP_SIZE;
 	this.height = MAP_SIZE;
-	this.level = 1;
+	this.level = 0;
 	this.score = 0;
 	this.totalAsteroidsThisLevel = 0;
 	this.currentAsteroidCount = 0;
+	this.ship = new SpaceShip();
+	this.pauseValues = {};
+	this.weaponBlasts = {
+		counter: 0,
+		list: {}
+	};
+	this.asteroids = {
+		counter: 0,
+		list: {}
+	};
+	this.messageBoard = {
+		start: '<h1>This webpage has been <em>invaided</em> by asteroids! Be a true Web Dev Hero and <em>destroy</em> those '+
+				'asteroids with your PULSE CANON!</h1><h1>User your arrow keys to navigate and the space bar to FIRE!</h1>'+
+				'<h1 id="start">Start</h1>',
+		end: '<h1>Good Job!<br>Your Final Score is:<br><span id="final_score">0</span></h1><h1 id="close">Close</h1>'
+	};
 }
-Map.prototype.dimensions = function() {
-	return {w: this.width, h: this.height};
-};
 Map.prototype.increaseScore = function() {
 	this.score += this.level;
 	$('#Score').text('Score: ' + this.score);
 };
 Map.prototype.repopulateAsteroids = function(asteroids) {
 	if (this.totalAsteroidsThisLevel < ASTEROID_POPULATION_SIZE * ASTEROID_WAVES) {
-		//add asteroid
+		this.totalAsteroidsThisLevel++;
+		this.asteroids.list['a' + this.totalAsteroidsThisLevel] = new Asteroid('a' + this.totalAsteroidsThisLevel, this.level);
 	}
-	else this.nextLevel();
+	else this.initializeLevel();
 };
-Map.prototype.nextLevel = function(asteroids) {
-
-};
+Map.prototype.initializeLevel = function() {
+	for (wbId in this.weaponBlasts.list) {
+		$('#'+wbId).remove();
+	}
+	for (aId in this.asteroids.list) {
+		$('#'+aId).remove();
+	}
+	this.ship.initialize();
+	this.level++;
+	$('#Level').text('Level: ' + this.level);
+	$('#Score').text('Score: ' + this.score);
+	this.totalAsteroidsThisLevel = 0;
+	this.currentAsteroidCount = 0;
+	this.weaponBlasts.counter = 0;
+	this.weaponBlasts.list = {};
+	this.asteroids.counter = 0;
+	this.asteroids.list = {};
+	this.pauseValues = {};
+	for(var i = 0; i < ASTEROID_POPULATION_SIZE; i++) {
+		this.totalAsteroidsThisLevel++;
+		this.asteroids.list['a' + this.totalAsteroidsThisLevel] = new Asteroid('a' + this.totalAsteroidsThisLevel, this.level);
+	}
+}
+Map.prototype.collisionTest = function() {
+	var collisionFound = false;
+	ssXMin = this.ship.position.x;
+	ssXMax = this.ship.position.x + SPACE_SHIP_SIZE;
+	ssYMin = this.ship.position.y;
+	ssYMax = this.ship.position.y + SPACE_SHIP_SIZE;
+	for (aId in this.asteroids.list) {
+		aXMin = this.asteroids.list[aId].position.x;
+		aXMax = this.asteroids.list[aId].position.x + ASTEROID_SIZE;
+		aYMin = this.asteroids.list[aId].position.y;
+		aYMax = this.asteroids.list[aId].position.y + ASTEROID_SIZE;
+		if (!(ssXMax < aXMin || aXMax < ssXMin || ssYMax < aYMin || aYMax < ssYMin)) {
+			this.level = 0;
+			this.score = 0;
+			this.initializeLevel();
+			collisionFound = true;
+			break;
+		}
+	}
+	collisionLoop: for (wbId in this.weaponBlasts.list) {
+		for (aId in this.asteroids.list) {
+			wbXMin = this.weaponBlasts.list[wbId].position.x;
+			wbXMax = this.weaponBlasts.list[wbId].position.x + WEAPON_BLAST_SIZE;
+			wbYMin = this.weaponBlasts.list[wbId].position.y;
+			wbYMax = this.weaponBlasts.list[wbId].position.y + WEAPON_BLAST_SIZE;
+			aXMin = this.asteroids.list[aId].position.x;
+			aXMax = this.asteroids.list[aId].position.x + ASTEROID_SIZE;
+			aYMin = this.asteroids.list[aId].position.y;
+			aYMax = this.asteroids.list[aId].position.y + ASTEROID_SIZE;
+			if (!(wbXMax < aXMin || aXMax < wbXMin || wbYMax < aYMin || aYMax < wbYMin)) {
+				this.weaponBlasts.list[wbId].evaporate(this);
+				this.asteroids.list[aId].evaporate(this);
+				this.currentAsteroidCount--;
+				this.repopulateAsteroids();
+				this.increaseScore();
+				collisionFound = true;
+				break collisionLoop;
+			}
+		}
+	}
+	if (collisionFound) {
+		$('#MessageBoard').html(this.messageBoard.end).css('left','50px');
+	}
+}
+Map.prototype.restart = function() {
+	this.level = 0;
+	this.score = 0;
+	this.initializeLevel();
+}
 
 function SpaceShip() {
 	this.acceleration = 1; //increase speed by 1px per second
 	this.fireRate = 1; //fire one shot per second
+	this.size = SPACE_SHIP_SIZE; //h: 20px, w: 20px
 	this.horizontalSpeed = 0; //pixels per second
 	this.verticalSpeed = 0; //pixels per second
 	this.position = { x: MAP_SIZE / 2 - SPACE_SHIP_SIZE / 2, y: MAP_SIZE / 2 - SPACE_SHIP_SIZE / 2 }; //center SpaceShip
 	this.weapons = 0; //0 of 3
 	this.shields = 0; //0 of 100
-	this.size = SPACE_SHIP_SIZE; //h: 20px, w: 20px
 	$('#SpaceShip').css({left: this.position.x + 'px', top: this.position.y + 'px'}); //place SpaceShip on Map
 }
+SpaceShip.prototype.initialize = function() {
+	this.horizontalSpeed = 0;
+	this.verticalSpeed = 0;
+	this.position = { x: MAP_SIZE / 2 - SPACE_SHIP_SIZE / 2, y: MAP_SIZE / 2 - SPACE_SHIP_SIZE / 2 };
+	this.weapons = 0;
+	this.shields = 0;
+	$('#SpaceShip').css({left: this.position.x + 'px', top: this.position.y + 'px'});
+};
 SpaceShip.prototype.move = function() {
 	this.position.x += this.horizontalSpeed;
 	if (this.position.x > MAP_SIZE) this.position.x = 0;
@@ -66,9 +157,9 @@ SpaceShip.prototype.accelerate = function(type) {
 			break;
 	}
 };
-SpaceShip.prototype.fire = function(weaponBlasts) {
-	weaponBlasts.counter++;
-	weaponBlasts.list['wb' + weaponBlasts.counter] = new WeaponBlast('wb' + weaponBlasts.counter, { x: this.position.x, y: this.position.y }, this.verticalSpeed);
+SpaceShip.prototype.fire = function(map) {
+	map.weaponBlasts.counter++;
+	map.weaponBlasts.list['wb' + map.weaponBlasts.counter] = new WeaponBlast('wb' + map.weaponBlasts.counter, { x: this.position.x, y: this.position.y }, this.verticalSpeed);
 };
 SpaceShip.prototype.upgradeWeapons = function() {
 	if (this.weapons < 3) this.weapons++;
@@ -86,14 +177,14 @@ function WeaponBlast(wbId, position, verticalSpeed) {
 	$('#Map').append('<div class="WeaponBlast" id="'+wbId+'"></div>');
 	$('#'+wbId).css({left: this.position.x + 'px', top: this.position.y + 'px'});
 }
-WeaponBlast.prototype.move = function(weaponBlasts) {
+WeaponBlast.prototype.move = function(map) {
 	this.position.y += this.verticalSpeed;
-	if (this.position.y < 0 - WEAPON_BLAST_SIZE) this.evaporate(weaponBlasts);
+	if (this.position.y < 0 - WEAPON_BLAST_SIZE) this.evaporate(map);
 	$('#' + this.id).css({top: this.position.y + 'px'});
 }
-WeaponBlast.prototype.evaporate = function(weaponBlasts) {
+WeaponBlast.prototype.evaporate = function(map) {
 	$('#' + this.id).remove();
-	delete weaponBlasts.list[this.id];
+	delete map.weaponBlasts.list[this.id];
 }
 
 function Asteroid(aId, level) {
@@ -131,7 +222,7 @@ Asteroid.prototype.move = function() {
 	else if (this.position.y < 0 - ASTEROID_SIZE) this.position.y = MAP_SIZE;
 	$('#' + this.id).css({left: this.position.x + 'px', top: this.position.y + 'px'});
 };
-Asteroid.prototype.evaporate = function(asteroids) {
+Asteroid.prototype.evaporate = function(map) {
 	$('#' + this.id).remove();
-	delete asteroids.list[this.id];
+	delete map.asteroids.list[this.id];
 }
